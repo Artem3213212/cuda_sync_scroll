729b1af484caf51604609337293382ff3a7f4afc
Artem3213212 <cx-1212@yandex.ru>
2018-07-04 01:08:50 +0300

    install.inf on_scroll

--- __init__.py
+++ __init__.py
@@ -5,38 +5,55 @@ import cudatext_cmd as cmds
 HorizontalScrol = False
 VerticalScrol = False
 
-class Command:
+def CallBackUpdate():
+    if VerticalScrol or HorizontalScrol:
+        if GROUPS_ONE==app_proc(PROC_GET_GROUPING,''):
+            app_proc(PROC_SET_EVENTS,__name__+';on_state;;')
+        else:
+            app_proc(PROC_SET_EVENTS,__name__+';on_scroll,on_state;;')
+    else:
+        app_proc(PROC_SET_EVENTS,__name__+';;;')
     
+
+class Command:
     def __init__(self):
         pass
 
     def config(self):
         pass
-        
+
     def Vertical(self):
         global VerticalScrol
         VerticalScrol=not VerticalScrol
+        CallBackUpdate()
 
     def Horizontal(self):
         global HorizontalScrol
         HorizontalScrol=not HorizontalScrol
+        CallBackUpdate()
 
     def on_scroll(self,ed_self):
         global HorizontalScrol
         global VerticalScrol
-        if VerticalScrol:
-            pos=ed_self.get_prop(PROP_SCROLL_VERT)
-            for h in ed_handles():
-                e=Editor(h)
-                e.set_prop(PROP_SCROLL_VERT,pos)
+        vpos=ed_self.get_prop(PROP_SCROLL_VERT)
+        hpos=ed_self.get_prop(PROP_SCROLL_HORZ)
+        n=ed_self.get_prop(PROP_INDEX_GROUP)
+        for i in range(6):
+            if i==n:
+                continue
+            e=ed_group(i)
+            if e!=None:
+                if VerticalScrol:
+                    e.set_prop(PROP_SCROLL_VERT,vpos)
+                if HorizontalScrol:
+                    e.set_prop(PROP_SCROLL_HORZ,hpos)
                 e.cmd(cmds.cmd_RepaintEditor)
-        if HorizontalScrol:
-            pos=ed_self.get_prop(PROP_SCROLL_HORZ)
-            for h in ed_handles():
-                e=Editor(h)
-                e.set_prop(PROP_SCROLL_HORZ,pos)
-                e.cmd(cmds.cmd_RepaintEditor)
-        
+                
+    def on_state(self, ed_self, state):
+        if state==APPSTATE_GROUPS:
+            CallBackUpdate()
+
+
 """
 AAAAAAAAAAAAAAAAAAA
 a 
