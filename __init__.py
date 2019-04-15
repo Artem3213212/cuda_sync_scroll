12a2dc1bcd1d00df7f455351f6c2ce8a6705d8fb
Alexey-t <support@uvviewsoft.com>
2019-04-15 20:04:44 +0300

    use new Cud API for smooth scrolling

--- __init__.py
+++ __init__.py
@@ -4,12 +4,12 @@ import cudatext_cmd as cmds
 
 def groups_ok():
     return app_proc(PROC_GET_GROUPING,'') in [GROUPS_2HORZ, GROUPS_2VERT]
-    
+
 
 class Command:
     sync_v = False
     sync_h = False
-    
+
     def __init__(self):
         pass
 
@@ -28,7 +28,7 @@ class Command:
             return msg_status('Cannot activate Sync Scroll for current groups mode')
         self.sync_v = not self.sync_v
         self.update()
-        
+
     def toggle_horz(self):
         if not groups_ok():
             return msg_status('Cannot activate Sync Scroll for current groups mode')
@@ -36,18 +36,38 @@ class Command:
         self.update()
 
     def on_scroll(self,ed_self):
-        vpos=ed_self.get_prop(PROP_SCROLL_VERT)
-        hpos=ed_self.get_prop(PROP_SCROLL_HORZ)
-        grp=ed_self.get_prop(PROP_INDEX_GROUP)
-        e=ed_group(1 if grp==0 else 0)
-        if e is not None:
-            if self.sync_v:
-                e.set_prop(PROP_SCROLL_VERT,vpos)
-            if self.sync_h:
-                e.set_prop(PROP_SCROLL_HORZ,hpos)
-            e.cmd(cmds.cmd_RepaintEditor)
-                
+
+        info_v = ed_self.get_prop(PROP_SCROLL_VERT_INFO)
+        info_h = ed_self.get_prop(PROP_SCROLL_HORZ_INFO)
+        pos_v = info_v['smooth_pos']
+        pos_h = info_h['smooth_pos']
+        max_v = info_v['smooth_pos_last']
+        max_h = info_h['smooth_pos_last']
+
+        grp = ed_self.get_prop(PROP_INDEX_GROUP)
+        e = ed_group(1 if grp==0 else 0)
+        if e is None: return
+
+        info2_v = e.get_prop(PROP_SCROLL_VERT_INFO)
+        info2_h = e.get_prop(PROP_SCROLL_HORZ_INFO)
+        max2_v = info2_v['smooth_pos_last']
+        max2_h = info2_h['smooth_pos_last']
+
+        #checks for max are needed to workaround CudaText bug, 
+        #with scroll at end with non-equal height files
+        if self.sync_v:
+            if pos_v<max_v and pos_v<max2_v:
+                e.set_prop(PROP_SCROLL_VERT_SMOOTH, pos_v)
+
+        if self.sync_h:
+            if pos_h<max_h and pos_h<max2_h:
+                e.set_prop(PROP_SCROLL_HORZ_SMOOTH, pos_h)
+
+        e.cmd(cmds.cmd_RepaintEditor)
+
+
     def on_state(self, ed_self, state):
+
         if state==APPSTATE_GROUPS:
             if not groups_ok():
                 self.sync_h = False
